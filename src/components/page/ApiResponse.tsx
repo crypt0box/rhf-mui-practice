2022ce0c79c5f246b66234cc8701e686f67dc09c
cryptobox <pray,happy76@gmail.com>
2022-12-04 16:25:17 +0900

    feat: 例を追加

--- src/components/page/ApiResponse.tsx
+++ src/components/page/ApiResponse.tsx
@@ -1,33 +1,59 @@
 import { Button } from "@mui/material";
 import axios from "axios";
 import { useState } from "react";
+import { z } from "zod";
 
-type Todo = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
+// type TodoResponse = {
+//   userId: string;
+//   id: boolean;
+//   title: number;
+//   completed: number;
+// };
+
+const TodoSchema = z.object({
+  userId: z.string(),
+  id: z.boolean(),
+  title: z.number(),
+  completed: z.number(),
+});
+
+type TodoResponse = z.infer<typeof TodoSchema>;
 
 export const ApiResponse = () => {
-  const [todo, setTodo] = useState<Todo>();
+  const [todo, setTodo] = useState<TodoResponse>();
   const onClickButton = async () => {
-    const response = await axios.get<Todo>(
+    const response = await axios.get(
       "https://jsonplaceholder.typicode.com/todos/1"
     );
-    setTodo(response.data);
+    try {
+      const data = TodoSchema.parse(response.data);
+      setTodo(data);
+    } catch (error) {
+      alert(error);
+    }
   };
   return (
     <>
       <Button onClick={onClickButton}>データ取得</Button>
       {todo && (
         <ul>
-          <li>userId: {todo.userId}</li>
+          {/* 👇本当はnumberなのにtoUpperCaseが使える */}
+          <li>userId: {todo.userId.toUpperCase()}</li>
           <li>id: {todo.id}</li>
           <li>title: {todo.title}</li>
-          <li>completed: {todo.completed ? "true" : "false"}</li>
+          {/* 👇本当はbooleanなのにtoFixedが使える */}
+          <li>completed: {todo.completed.toFixed(2) ? "true" : "false"}</li>
         </ul>
       )}
     </>
   );
 };
+
+/**
+ * response.data : {
+ *  userId: number;
+ *  id: number;
+ *  title: string;
+ *  completed: boolean;
+ * }
+ */
