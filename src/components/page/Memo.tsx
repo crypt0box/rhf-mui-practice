2abf9c7b0d034bdf95dbc326c57502050e200a28
cryptobox <pray.happy76@gmail.com>
2023-07-11 23:03:19 +0900

    initial commit

--- src/components/page/Memo.tsx
+++ src/components/page/Memo.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Box, Button } from '@mui/material';
 import { useCallback, useMemo, useState } from 'react';
 
@@ -5,12 +6,17 @@ export const Memo = () => {
   const [rightNum, setRightNum] = useState(0);
   const [leftNum, setLeftNum] = useState(0);
 
+  // コンポーネントをメモ化してもpropsの関数もメモ化しないと再レンダリングされてしまう
   const rightCountUp = useCallback(() => {
     setRightNum((prev) => prev + 1);
   }, [setRightNum]);
 
-  const leftCountUp = () => setLeftNum((prev) => prev + 1);
+  const leftCountUp = useCallback(() => {
+    setLeftNum((prev) => prev + 1);
+  }, [setLeftNum]);
 
+  // 恐らくuseMemoでもReact.memoでもコンポーネントのメモ化は可能
+  // React.memoのほうがimport側で型が効くし依存配列を書かなくて良いから上？
   const RCard = useMemo(
     () => <RightCard rightNum={rightNum} rightCountUp={rightCountUp} />,
     [rightNum, rightCountUp]
@@ -44,16 +50,15 @@ const RightCard: React.FC<{ rightNum: number; rightCountUp: () => void }> = ({
   );
 };
 
-const LeftCard: React.FC<{ leftNum: number; leftCountUp: () => void }> = ({
-  leftNum,
-  leftCountUp,
-}) => {
-  return (
-    <Box
-      sx={{ padding: '16px', border: '1px solid black', borderRadius: '4px' }}
-    >
-      <Box sx={{ textAlign: 'center' }}>{leftNum}</Box>
-      <Button onClick={leftCountUp}>up</Button>
-    </Box>
-  );
-};
+const LeftCard = React.memo(
+  (props: { leftNum: number; leftCountUp: () => void }) => {
+    return (
+      <Box
+        sx={{ padding: '16px', border: '1px solid black', borderRadius: '4px' }}
+      >
+        <Box sx={{ textAlign: 'center' }}>{props.leftNum}</Box>
+        <Button onClick={props.leftCountUp}>up</Button>
+      </Box>
+    );
+  }
+);
